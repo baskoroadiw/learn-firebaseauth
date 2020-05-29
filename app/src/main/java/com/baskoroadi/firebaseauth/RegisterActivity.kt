8fdc9ad6c75227a9bfbd01f48621e7bca0440b41
Baskoro Adi <baskoro.baskoro.adi5@gmail.com>
2020-05-29 16:59:03 +0700

    Membuat fungsi register

--- app/src/main/java/com/baskoroadi/firebaseauth/RegisterActivity.kt
+++ app/src/main/java/com/baskoroadi/firebaseauth/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.baskoroadi.firebaseauth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.razir.progressbutton.attachTextChangeAnimator
+import com.github.razir.progressbutton.bindProgressButton
+import com.github.razir.progressbutton.hideProgress
+import com.github.razir.progressbutton.showProgress
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import kotlinx.android.synthetic.main.layout_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +27,58 @@ class RegisterActivity : AppCompatActivity() {
         txtHaveAccount.setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))
         }
+
+        auth = FirebaseAuth.getInstance()
+
+        buttonRegister.setOnClickListener {
+            signUp()
+        }
+
+    }
+
+    private fun signUp(){
+        startProgress()
+        val email = edittext_signup_email.text.toString().trim()
+        val pass = edittext_signup_pass.text.toString().trim()
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(this, "Lengkapi Isian", Toast.LENGTH_SHORT).show()
+        }else{
+            signUpProcess(email, pass)
+        }
+    }
+
+    private fun signUpProcess(email:String, pass:String){
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    stopProgress()
+                    Toast.makeText(this, "Register Sukses", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,LoginActivity::class.java))
+                } else {
+                    stopProgress()
+                    if (task.exception is FirebaseAuthWeakPasswordException){
+                        Toast.makeText(this, "Password minimal 6 karakter", Toast.LENGTH_SHORT).show()
+                    }else if (task.exception is FirebaseAuthInvalidCredentialsException){
+                        Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show()
+                    }else if (task.exception is FirebaseAuthUserCollisionException){
+                        Toast.makeText(this, "Email sudah terdaftar", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+    private fun startProgress(){
+        bindProgressButton(buttonRegister)
+        buttonRegister.attachTextChangeAnimator()
+
+        buttonRegister.showProgress{
+            buttonTextRes = R.string.login_loading
+            progressColor = Color.WHITE
+        }
+    }
+
+    private fun stopProgress(){
+        buttonRegister.hideProgress(R.string.register)
     }
 }
