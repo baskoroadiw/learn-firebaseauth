ea54d8dde85e85f88069367a3c40f9d105cc7fb1
Baskoro Adi <baskoro.baskoro.adi5@gmail.com>
2020-05-30 16:32:08 +0700

    Menambahkan progress button pada forgot password

--- app/src/main/java/com/baskoroadi/firebaseauth/ForgotPasswordActivity.kt
+++ app/src/main/java/com/baskoroadi/firebaseauth/ForgotPasswordActivity.kt
@@ -1,10 +1,15 @@
 package com.baskoroadi.firebaseauth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.razir.progressbutton.attachTextChangeAnimator
+import com.github.razir.progressbutton.bindProgressButton
+import com.github.razir.progressbutton.hideProgress
+import com.github.razir.progressbutton.showProgress
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.layout_forgot_password.*
 
@@ -31,14 +36,30 @@ class ForgotPasswordActivity : AppCompatActivity() {
             editTextEmail_forgot.error = "Harus diisi"
         }
         else{
+            startProgress()
             auth.sendPasswordResetEmail(email)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    stopProgress()
                     Toast.makeText(this, "Email terkirim, mohon cek email anda", Toast.LENGTH_SHORT).show()
                     finish()
                     startActivity(Intent(this,LoginActivity::class.java))
                 }
             }
         }
+    }
+
+    private fun startProgress(){
+        bindProgressButton(button_send_email_forgot)
+        button_send_email_forgot.attachTextChangeAnimator()
+
+        button_send_email_forgot.showProgress{
+            buttonTextRes = R.string.login_loading
+            progressColor = Color.WHITE
+        }
+    }
+
+    private fun stopProgress(){
+        button_send_email_forgot.hideProgress(R.string.forgot_text)
     }
 }
